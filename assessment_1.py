61856c059b065d880b32dc1626c65c848bb01670
Obinna Ofomah <obinna.ofomah@outlook.com>
2024-03-05 13:43:33 +0000

    completed assessment 1

--- assessment_1.py
+++ assessment_1.py
@@ -11,3 +11,10 @@ This is a self assessment session and coding should be recorded using loom.
 
 Good luck
 """
+
+word1 = "faith"
+word2 = "alpha"
+word3 = "ten"
+
+first_letters = word1[0] + word2[0] + word3[0]
+print(first_letters)
